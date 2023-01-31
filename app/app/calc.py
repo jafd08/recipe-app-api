3ac707da9400a585e08969446c13ab80951f529d
Jesus Fung <jesusfd988work@gmail.com>
2023-01-31 06:35:45 -0600

    Finished section 8: 40 (write tests for wait_for_db command)

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,10 +3,11 @@ Calculator functions
 """
 
 
-def add(x,y):
-    """ Add x and y and return result """
+def add(x, y):
+    """Add x and y and return result"""
     return x + y
 
-def substract(x,y):
-    """ Substract x from y and return result """
+
+def substract(x, y):
+    """Substract x from y and return result"""
     return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -3,12 +3,15 @@ Sample Tests
 """
 
 from django.test import SimpleTestCase
+
 from app import calc
+
 
 class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
         self.assertEqual(res, 11)
+
     def test_subract_numbers(self):
-        res = calc.substract(10,15)
+        res = calc.substract(10, 15)
         self.assertEqual(res, 5)
