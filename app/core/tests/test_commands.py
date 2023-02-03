4e38587b9c49c03567b8bec700d738ab6d1c425e
Jesus Fung <jesusfd988work@gmail.com>
2023-02-03 00:26:19 -0600

    Configured docker compose to check on DB before doing any test or runserver

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -18,18 +18,18 @@ class CommandTests(SimpleTestCase):
         """Test waiting for db if db ready"""
         patched_check.return_value = True
         call_command("wait_for_db")
-        patched_check.assert_called_once_with(database=["default"])
+        patched_check.assert_called_once_with(databases=["default"])
 
     @patch("time.sleep")
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """
         Test waiting for database when getting OperationalError
-        First param is the closes one, and the farthest one is the last
-        parameter
+        First param is the closes one (@patch), and the farthest one is
+        the last parameter (wait for db Command check)
         """
         patched_check.side_effect = (
             [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
         )
         call_command("wait_for_db")
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=["default"])
+        patched_check.assert_called_with(databases=["default"])
